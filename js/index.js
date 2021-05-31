3e978d73f7db43f32086f8a0eb2f7ecadc7194b7
PriyanshuKapadia <priyanshu.kapadia@gmail.com>
2021-05-31 15:06:07 +0530

    Version 1.0

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = ()=>{
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+  body.classList.add("disabled");
+}
+cancelBtn.onclick = ()=>{
+  body.classList.remove("disabled");
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+}
+window.onscroll = ()=>{
+  this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
+
+var typed = new Typed('.type',{
+  strings:['Web Developer.','Creative Designer.','Flutter Developer.','Freelancer.','Priyanshu Kapadia.'],
+  typeSpeed: 60,
+  backSpeed: 60,
+  loop:true,
+});
